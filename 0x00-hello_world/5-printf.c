1818a17d22d6cdfa7af96a98a34aa262bf99a58d
Sherifsalam <sherifehab205@icloud.com>
2023-07-13 18:35:51 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with printf function.
+ * Description: A C program that prints with put function
  *
- * Return : Always 0 (Success)
+ * Return: Alawys 0 (Success)
 */
-int main (void)
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
