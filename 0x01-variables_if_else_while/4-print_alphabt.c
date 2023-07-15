4e83793522b1c8455a8e3ff09a8435ac3c3670b3
Sherifsalam <sherifehab205@icloud.com>
2023-07-14 18:20:37 -0700

    e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 
-        while (ch <= 'z')
-        {
-                if (ch == 'e' || ch == 'q')
-                        ch++;
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		ch++;
 
-                putchar(ch);
-                ch++;
+		putchar(ch);
+		ch++;
         }
-        putchar('\n');
+	putchar('\n');
 
         return (0);
 }
