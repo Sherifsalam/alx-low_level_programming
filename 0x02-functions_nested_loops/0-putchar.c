7556b3428b39692271334aba48a9858d52d2e222
Sherifsalam <sherifehab205@icloud.com>
2023-07-18 15:19:07 -0700

    e

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:this program print putchar with new line
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+}
+	printf("putchar\n");
+	return (0);
+}
+
