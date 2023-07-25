0b89c108f6da99107b9bdcbbecf7356e9a98a7e7
Sherifsalam <sherifehab205@icloud.com>
2023-07-25 13:12:16 -0700

    e

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,9 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
+
 
 #include<stdio.h>
-
-int _putchar(char c)
-void reset_to_98(int *n); 
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -16,5 +15,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 
-#endif /* MAIN.H */
-
+#endif /* MAIN_H */
