a6b6acbd2c163cd80a856e911d3941d54e94ff40
Sherifsalam <sherifehab205@icloud.com>
2023-08-04 19:06:15 -0700

    e

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int _isdigit(int c);
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, k, sum = 0;
 
 	if (argc == 1)
 		printf("0\n");
@@ -23,8 +23,12 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (_isdigit(argv[i][0]))
-				sum += atoi(argv[i]);
+			k = _isdigit(argv[i][0]);
+			if (k)
+			{
+				if (k >= 0)
+					sum += atoi(argv[i]);
+			}
 			else
 			{
 				printf("Error\n");
